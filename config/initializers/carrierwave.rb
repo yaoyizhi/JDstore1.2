4a945e3c0e41d2fffa9a8f02383eb373f02e2b80
yaoyizhi <email@yaoadou888>
2017-05-25 20:24:33 +0800

    modify qiniu website

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,10 @@
 CarrierWave.configure do |config|
     config.storage = :qiniu
-    config.qiniu_access_key    = ENV['jqwIb52HuLYjr8SyN91YmQ3hPH-60t5JB2CaqhS5']
-    config.qiniu_secret_key    = ENV['J7vus-v5mODJLD_BSyHBtDuSzcEN3QQ50N9MuftY']
-    config.qiniu_bucket        = ENV['jdstore-demo']
-    config.qiniu_bucket_domain = ENV["
-  om45b7pwl.bkt.clouddn.com"]
+    config.qiniu_access_key    = ENV['qiniu_access_key']
+    config.qiniu_secret_key    = ENV['qiniu_secret_key']
+    config.qiniu_bucket        = ENV['qiniu_bucket']
+    config.qiniu_bucket_domain = ENV['qiniu_bucket_domain']
     config.qiniu_block_size    = 4 * 1024 * 1024
     config.qiniu_protocol      = 'http'
-    config.qiniu_up_host       = 'http://up.qiniug.com' # 选择不同的区域时，"up.qiniug.com" 不同
+    config.qiniu_up_host       = 'http://up-z1.qiniu.com' # 选择不同的区域时，"up.qiniug.com" 不同
 end
