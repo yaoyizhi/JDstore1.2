927a70f8b9a45c285e2e01293e0f929125fb261c
yaoyizhi <email@yaoadou888>
2017-06-06 16:12:15 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,8 @@ u.password_confirmation = '123456'
 u.is_admin = true
 u.save
 products_info = [
-    { title: '赣南脐橙', description: '朋友产地直销的，吃过真心不错，不打保鲜剂的推荐给大家 16楼2单元团圆爸爸推荐，家里还有两箱，欢迎晚上过来品尝。', image: 'https://ws3.sinaimg.cn/large/006tNc79gy1fgbcdhps28j307n053747.jpg' },
-    { title: '凤凰岭樱桃', description: '5月28号去采摘过，个大甜，有两个品种，老板给每斤25元算合适，11号楼3单元甜甜妈推荐', image: 'https://ws2.sinaimg.cn/large/006tNc79gy1fgbcmaa302j307p0523yl.jpg' },
+    { title: '赣南脐橙', description: '朋友产地直销的，吃过真心不错，不打保鲜剂的推荐给大家 16楼2单元团圆爸爸推荐，家里还有两箱，欢迎晚上过来品尝。' },
+    { title: '凤凰岭樱桃', description: '5月28号去采摘过，个大甜，有两个品种，老板给每斤25元算合适，11号楼3单元甜甜妈推荐' },
     { title: '手工钩针课', description: '教材如图所示，每周一次课，具体时间根据学员时间调整,7号楼邮政局楼上，联系6号楼3单元米兰妈妈' },
     { title: '小学生放学托管', description: '收4个小学生，下午3-6点，督促作业，包晚饭和水果，8号楼1单元嘟嘟家' },
     { title: '手工包子，饺子', description: '可自己准备食材，到家做，也可我们做好了，送过去，16号楼1单元美美家' }
@@ -24,7 +24,7 @@ products_info.each do |info|
         title: info[:title],
         description: info[:description],
         quantity: rand(5..6),
-        price: rand(5..6) * 10,
-        image: open(info[:image])
+        price: rand(5..6) * 10
+
     )
 end
